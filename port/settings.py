a0b7c1f2f409a1e185c97701057842bbc57a524e
KARTIKEY KAKRAN <kartikey kakran>
2021-08-29 22:15:59 +0530

    host

--- port/settings.py
+++ port/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 STATICFILES_DIRS= [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
